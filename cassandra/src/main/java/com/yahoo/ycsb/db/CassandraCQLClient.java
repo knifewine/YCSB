ac9ba91041d34248efc488f1b0cc61dfa7de1963
Jonathan Ellis <jbellis@gmail.com>
2014-10-09 17:59:37 -0500

    Revert "restrict to single connection"
    This reverts commit 51f2720995db55c3d26559b170d0daed26bd0684.

--- cassandra/src/main/java/com/yahoo/ycsb/db/CassandraCQLClient.java
+++ cassandra/src/main/java/com/yahoo/ycsb/db/CassandraCQLClient.java
@@ -128,9 +128,7 @@ public class CassandraCQLClient extends DB
 
             Cluster.Builder builder = Cluster.builder()
                                              .withPort(Integer.valueOf(port))
-                                             .addContactPoints(hosts)
-                                             .withPoolingOptions(new PoolingOptions().setCoreConnectionsPerHost(HostDistance.LOCAL, 1)
-                                                                                     .setMaxConnectionsPerHost(HostDistance.LOCAL, 1));
+                                             .addContactPoints(hosts);
             if ((username != null) && !username.isEmpty())
             {
                 builder = builder.withCredentials(username, password);
