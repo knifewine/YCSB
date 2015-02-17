fab55548e46b408d4e325e110d63fb641efe75c7
russ hatch <rhatch@datastax.com>
2015-02-17 23:40:50 +0000

    changes for cassandra-8692

--- cassandra.cql.load.ycsb.sh
+++ cassandra.cql.load.ycsb.sh
@@ -5,18 +5,18 @@ TRIAL=`date +%Y-%m-%d_%H-%M-%S`
 
 echo "drop keyspace if exists usertable;
 drop keyspace if exists ycsb;
-create keyspace ycsb WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 2 };
+create keyspace ycsb WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 3 };
 create table ycsb.usertable (
     y_id varchar primary key,
     field0 blob,
     field1 blob,
     field2 blob,
     field3 blob,
-    field4 blob) WITH compression = {'sstable_compression':''};
+    field4 blob);
 " | ~/fab/cassandra/bin/cqlsh node0
 
 sleep 10
 
 echo "Load..."
-bin/ycsb load cassandra-cql -s -P workloads/ryan_couchbase > trials/cassandra/cass.$TRIAL.cql.load.out 2> trials/cassandra/cass.$TRIAL.cql.load.err
+bin/ycsb load cassandra-cql -s -P workloads/russ_8692 > trials/cassandra/cass.$TRIAL.cql.load.out 2> trials/cassandra/cass.$TRIAL.cql.load.err
 #bin/ycsb load cassandra-cql -s -P workloads/test > trials/cassandra/cass.$TRIAL.cql.load.out 2> trials/cassandra/cass.$TRIAL.cql.load.err
