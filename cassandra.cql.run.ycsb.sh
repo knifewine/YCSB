2b6c734d7496858c2a39e75924f1e2b33103a882
russ hatch <rhatch@datastax.com>
2015-02-19 18:09:44 +0000

    don't override distribution in run script

--- cassandra.cql.run.ycsb.sh
+++ cassandra.cql.run.ycsb.sh
@@ -3,8 +3,6 @@
 mkdir -p trials/cassandra
 TRIAL=`date +%Y-%m-%d_%H-%M-%S`
 
-DISTRIBUTION="uniform"
-
 echo "Run..."
 bin/ycsb run cassandra-cql -s -P workloads/russ_8692 > trials/cassandra/cass.$TRIAL.cql.$DISTRIBUTION.run.out 2> trials/cassandra/cass.$TRIAL.cql.$DISTRIBUTION.run.err
 
